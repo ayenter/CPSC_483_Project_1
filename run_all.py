9b1a10270192c9a32d7eb32dfbbb24302134544c
Alec Yenter <Batman@Alecs-MacBook.attlocal.net>
2016-10-23 00:59:44 -0700

    Added: file to run all algorithms

---
+++ run_all.py
@@ -0,0 +1,14 @@
+# ~~~ Author: Alec Yenter ~~~
+
+print("\n *** *** *** *** ****************** *** *** *** *** ")
+print(" *** *** *** *** KNN CLASSIFICATION *** *** *** *** ")
+import kNN_classification
+print("\n *** *** *** *** ************** *** *** *** *** ")
+print(" *** *** *** *** KNN REGRESSION *** *** *** *** ")
+import kNN_regression
+print("\n *** *** *** *** ***************** *** *** *** *** ")
+print(" *** *** *** *** LINEAR REGRESSION *** *** *** *** ")
+import linear_regression
+print("\n *** *** *** *** *********** *** *** *** *** ")
+print(" *** *** *** *** NAIVE BAYES *** *** *** *** ")
+import naive_bayes
